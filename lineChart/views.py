e4103243dd324cb6548311acdaa2e70d7ed8b374
Kshitij Kumar Jha <95013878+techkshitij92@users.noreply.github.com>
2022-01-28 14:11:16 +0530

    Updated views
    Added snippet to pass code to database.

--- lineChart/views.py
+++ lineChart/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from . models import T1Score, T2Score
-
 # Create your views here.
 
 def index(request):
-    data1 = T1Score.objects.all()
-    data2 = T2Score.objects.all()
-
-    context = {
+      ins1 = T1Score(Level="lvl1",point=50)
+      ins1.save()
+      ins2 = T2Score(Level="lvl1",point=40)
+      ins2.save()
+      data1=T1Score.objects.all()
+      data2=T2Score.objects.all()
+      context = {
         "data1": data1,
-        "data2": data2
-    }
-
-    return render(request, 'lineChart/index.html', context)
+        "data2": data2,
+      }
+      print(context['data1'])
+      print(context)
+      return render(request, 'lineChart/index.html', context)
